d8d35f30ab76f8b1119161fd2b4bdf586502b579
EngHamzaAboJarad <flutter.dart.2021.new@gmail.com>
2022-09-01 15:13:21 +0300

    UpdateGenerateRoute

--- lib/Screens/START _LOGIN/rout_onGenerateRout.dart
+++ lib/Screens/START _LOGIN/rout_onGenerateRout.dart
@@ -1,17 +1,24 @@
+import 'dart:developer';
+
+import 'package:abllseducation/Screens/START%20_LOGIN/PageView.dart';
 import 'package:abllseducation/Splash_Screen.dart';
 import 'package:flutter/material.dart';
 
 class routapp{
   static  const String initialRoute = '/';
-  static  const String HomeRoutScreen = '/homeScreen';
+  static  const String pageViewRoutScreen = '/PageViewScreen';
 }
 class AppRouts{
   static Route? OnGenerateRout (RouteSettings routeSettings){
     switch(routeSettings.name){
       case   routapp.initialRoute:
+        log(routeSettings.name.toString());
         return MaterialPageRoute(builder: (context) => const SplashScreen());
-      case   routapp.HomeRoutScreen:
-         return MaterialPageRoute(builder: (context) => const Scaffold(),);
+      case   routapp.pageViewRoutScreen:
+        log(routeSettings.name.toString());
+        return MaterialPageRoute(builder: (context) =>  const  PageViewScreen(),);
+      default:
+        return null;
     }
   }
 }
--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -1,3 +1,4 @@
+import 'package:abllseducation/Screens/START%20_LOGIN/rout_onGenerateRout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 class SplashScreen extends StatefulWidget {
@@ -9,16 +10,15 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    Future.delayed(Duration(seconds: 3),() => Navigator.pushReplacementNamed(context, routapp.pageViewRoutScreen));
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-          ],
-        ),
-      ),
+       body:Center(child: Image.asset('assets/logo.png')),
     );
   }
 }
