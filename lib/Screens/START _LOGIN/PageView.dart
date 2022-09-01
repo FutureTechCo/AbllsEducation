f8d7647dcac19f57c69ef5e2bf6b9c59a38f6603
EngHamzaAboJarad <flutter.dart.2021.new@gmail.com>
2022-09-01 17:38:15 +0300

    PageViews

--- lib/Screens/START _LOGIN/PageView.dart
+++ lib/Screens/START _LOGIN/PageView.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:abllseducation/Utils/StylePageView.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -9,29 +12,197 @@ class PageViewScreen extends StatefulWidget {
 }
 
 class _PageViewScreenState extends State<PageViewScreen> {
+ late PageController pageController;
+ int _pageNumber = 0;
+ @override
+  void initState() {
+    pageController = PageController();// TODO: implement initState
+    super.initState();
+  }
+  @override
+  void dispose() {
+    pageController.dispose();// TODO: implement dispose
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-          child: Image.asset(
-            'assets/page1.png',
-            width: 204.w,
-            height: 246.h,
+    body: Padding(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _pageNumber!=0?IconButton(onPressed: (){
+            pageController.previousPage(duration:const Duration(microseconds:  5), curve: Curves.easeInOut);
+          }, icon: Icon(Icons.arrow_back,color: Colors.blue,)):SizedBox(height:46.h,),
+          Container(
+            height: 600.h,
+             width: double.infinity,
+            child: Center(
+              child: PageView(
+                controller: pageController,
+                onPageChanged: (int i){
+                  setState((){
+                    _pageNumber = i;
+                  });
+                },
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0.h),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            child: Image.asset(
+                              'assets/page1.png',
+                              width: 204.w,
+                              height: 246.h,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 56.h,
+                          ),
+                          Text('نبذه تعريفيه 1', style: StyleTile()),
+                          SizedBox(
+                            height: 26.h,
+                          ),
+                          Text(
+                              'يدعم برنامج الإيبلز أنشطه العلاج السلوكي ويساعدك في تحسين مهارات اللغة لدى طفلك',
+                              textAlign: TextAlign.center,
+                              style: StyleSupTile()),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0.h),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          // Align(alignment: Alignment.topLeft,child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back,color: Color(0xff003298),))),
+                          //  SizedBox(
+                          //    height: 26.h,
+                          //  ),
+                          Container(
+                            child: Image.asset(
+                              'assets/page2.png',
+                              width: 204.w,
+                              height: 246.h,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 56.h,
+                          ),
+                          Text('نبذة تعريفيه 2', style: StyleTile()),
+                          SizedBox(
+                            height: 26.h,
+                          ),
+                          Text(
+                            'يتيح لك التطبيق فرصه تقيم التدريب ومتابعه الخطه بذاتك كأن معك متخصص في بيتك',
+                            style: StyleSupTile(),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0.h),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          // Align(alignment: Alignment.topLeft,child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back,color: Color(0xff003298),))),
+                          // SizedBox(
+                          //   height: 26.h,
+                          // ),
+                          Container(
+                            child: Image.asset(
+                              'assets/page3.png',
+                              width: 204.w,
+                              height: 246.h,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 56.h,
+                          ),
+                          Text('نبذة تعريفيه 3', style: StyleTile()),
+                          SizedBox(
+                            height: 26.h,
+                          ),
+                          Text(
+                            'لن تحتاج للاستفسار كثيراً كيفيه وطريقه تنفيذ النشاط ستجد كل نشاط له فيديو يشرح طريقه تطبيقه داخل مجاله بإمكاك الإطلاع على أي فيديو وإعادة مشاهده متى إحتجت ذلك',
+                            style: StyleSupTile(),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-        SizedBox(
-          height: 56.h,
-        ),
-        Text('موضوع'),
-        SizedBox(
-          height: 56.h,
-        ),
-        Text('تطبيق مخصص لتعليم الأطفال من قبل'
-            'المدربين او الوالدين وتقييم الطفل'
-            'من خلال التطبيق '),
-      ],
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              width: 189.w,
+              height: 50.h,
+               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                    Container(
+                      width: _pageNumber!=0?10.w:27.w,
+                      height: 10.h,
+                      decoration: BoxDecoration(
+                          color: _pageNumber!=0?Colors.white:Colors.blue,
+                          borderRadius: BorderRadius.circular(90.r),
+                          border: Border.all(
+                              color:Colors.blue
+                          )),
+                    ),
+                    SizedBox(
+                      width: 10.w,
+                    ),
+                    Container(
+                      width: _pageNumber!=1?10.w:27.w,
+                      height: 10.h,
+                      decoration: BoxDecoration(
+                          color: _pageNumber!=1?Colors.white:Colors.blue,
+                          borderRadius: BorderRadius.circular(90.r),
+                          border: Border.all(
+                              color: Colors.blue
+                          )
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10.w,
+                    ),
+                    Container(
+                      width: _pageNumber!=2?10.w:27.w,
+                      height: 10.h,
+                      decoration: BoxDecoration(
+                          color: _pageNumber!=2?Colors.white:Colors.blue,
+                          borderRadius: BorderRadius.circular(90.r),
+                          border: Border.all(
+                              color: Colors.blue
+                          )
+                      ),
+                    )
+                  ],),
+                  FloatingActionButton(
+                      onPressed: (){
+                        pageController.nextPage(duration:const Duration(microseconds:  5), curve: Curves.easeInOut);
+                      },
+                      child:Icon(Icons.arrow_forward_ios))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     ));
   }
 }
---
+++ lib/Utils/StylePageView.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+ TextStyle  StyleTile() {
+  return TextStyle(
+      fontFamily: 'Arial', fontWeight: FontWeight.bold, fontSize: 25.sp);
+}
+
+TextStyle StyleSupTile() {
+  return TextStyle(
+      fontFamily: 'Arial', fontWeight: FontWeight.w400, fontSize: 20.sp);
+}
